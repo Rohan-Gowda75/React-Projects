9cb00ca479eb38e7a2735c20f062d9c3b8366f4f
Rohan gowda <rohangowdaka@gmail.com>
2025-10-08 15:59:33 +0530

    Add files via upload

---
+++ ai-resume-analyzer/app/routes/home.tsx
@@ -0,0 +1,69 @@
+import type { Route } from "./+types/home";
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
+import { Link } from "react-router";
+import { useEffect, useState } from "react";
+import { resumes as staticResumes } from "~/constants/index";
+
+export function meta({}: Route.MetaArgs) {
+    return [
+        { title: "hired.IN" },
+        { name: "description", content: "Smart feedback for your dream job!" },
+    ];
+}
+
+export default function Home() {
+
+    const [resumes, setResumes] = useState<Resume[]>([]);
+    const [loadingResumes, setLoadingResumes] = useState(false);
+
+
+    useEffect(() => {
+        setLoadingResumes(true);
+
+
+        setTimeout(() => {
+            setResumes(staticResumes);
+            setLoadingResumes(false);
+        }, 300); // 300ms delay
+    }, []);
+
+    return (
+        <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+            <Navbar />
+
+            <section className="main-section">
+                <div className="page-heading py-16">
+                    <h1>Evaluate Your Resume & Enhance Your Impact</h1>
+                    {!loadingResumes && resumes.length === 0 ? (
+                        <h2>No resumes found. Upload your first resume to get feedback.</h2>
+                    ) : (
+                        <h2>Review your submissions and check AI-powered feedback.</h2>
+                    )}
+                </div>
+
+                {loadingResumes && (
+                    <div className="flex flex-col items-center justify-center">
+                        <img src='/images/resume-scan-2.gif' className="w-[200px]" />
+                    </div>
+                )}
+
+                {!loadingResumes && resumes.length > 0 && (
+                    <div className="resumes-section">
+                        {resumes.map((resume) => (
+                            <ResumeCard key={resume.id} resume={resume} />
+                        ))}
+                    </div>
+                )}
+
+                {!loadingResumes && resumes.length === 0 && (
+                    <div className="flex flex-col items-center justify-center mt-10 gap-4">
+                        <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+                            Upload Resume
+                        </Link>
+                    </div>
+                )}
+            </section>
+        </main>
+    );
+}
